f3e75787e1b52b396e8fe4b94e53cfed6ca00c55
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-07-02 00:09:08 +0200

    update design

--- alarm/lib/src/sample_feature/alarm_screen.dart
+++ alarm/lib/src/sample_feature/alarm_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slidable_button/slidable_button.dart';
 
 class AlarmScreen extends StatefulWidget {
   final String? payload;
@@ -40,40 +41,81 @@ class _AlarmScreenState extends State<AlarmScreen> {
         children: <Widget>[
           Expanded(
             child: Center(
-              child: Text(
-                _formatDateTime(DateTime.now()),
-                style: TextStyle(fontSize: 30, color: Colors.white),
-                textAlign: TextAlign.center,
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // center vertically
+                children: [
+                  Text(
+                    _formatDateTime(DateTime.now()),
+                    style: TextStyle(fontSize: 90, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'Alarm',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  const Text('Slide this button to left or right.'),
+                  const SizedBox(height: 64.0),
+                  HorizontalSlidableButton(
+                    initialPosition: SlidableButtonPosition.center,
+                    height: MediaQuery.of(context).size.height / 10,
+                    width: MediaQuery.of(context).size.width / 1.25,
+                    buttonWidth: MediaQuery.of(context).size.width / 5,
+                    color: Theme.of(context)
+                        .colorScheme
+                        .secondary
+                        .withOpacity(0.5),
+                    buttonColor: Theme.of(context).primaryColor,
+                    dismissible: false,
+                    label: const Center(child: Text('Slide Me')),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Snooze'),
+                          Text('Drag Me'),
+                          Text('Stop'),
+                        ],
+                      ),
+                    ),
+                    onChanged: (position) {
+                      setState(() {
+                        if (position == SlidableButtonPosition.end) {
+                          // result = 'Button is at the right';
+                        } else {
+                          // result = 'Button is on the left';
+                        }
+                      });
+                    },
+                  ),
+                ],
               ),
             ),
           ),
-          Text(
-            'Alarm',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-          SliderTheme(
-            data: SliderThemeData(
-              trackHeight: 4.0,
-              thumbColor: Colors.blue,
-              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 20.0),
-              overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-            ),
-            child: Slider(
-              value: _dragValue,
-              onChanged: (newValue) {
-                setState(() {
-                  _dragValue = newValue;
-                });
-              },
-              onChangeEnd: _onDragEnd,
-              min: -1.0,
-              max: 1.0,
-              divisions: 2,
-              label: _dragValue < 0 ? 'Snooze' : 'Stop',
-              activeColor: _dragValue < 0 ? Colors.blue : Colors.red,
-            ),
-          ),
+          //   SliderTheme(
+          //     data: SliderThemeData(
+          //       trackHeight: 4.0,
+          //       thumbColor: Colors.blue,
+          //       thumbShape: RoundSliderThumbShape(enabledThumbRadius: 20.0),
+          //       overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+          //     ),
+          //     child: Slider(
+          //       value: _dragValue,
+          //       onChanged: (newValue) {
+          //         setState(() {
+          //           _dragValue = newValue;
+          //         });
+          //       },
+          //       onChangeEnd: _onDragEnd,
+          //       min: -1.0,
+          //       max: 1.0,
+          //       divisions: 2,
+          //       label: _dragValue < 0 ? 'Snooze' : 'Stop',
+          //       activeColor: _dragValue < 0 ? Colors.blue : Colors.red,
+          //     ),
+          //   ),
         ],
       ),
     );
